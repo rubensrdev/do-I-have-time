7a81e91f59e9a2ba704b56362d8a584f7fc55639
Rubén <ruben.sr.apple@gmail.com>
2024-04-22 13:01:11 +0200

    layout title and info interface

--- doihavetime/doihavetime/ContentView.swift
+++ doihavetime/doihavetime/ContentView.swift
@@ -10,10 +10,18 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Do I Have Time?")
+                .font(.title2)
+                .bold()
+                .foregroundStyle(.myPurple)
+                .padding()
+            
+            Text("I help you know if you will have time to watch that movie or episode of your favorite series")
+                .font(.subheadline)
+                .foregroundStyle(.gray)
+            
+            
+            Spacer()
         }
         .padding()
     }

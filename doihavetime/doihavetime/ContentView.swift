1779757db947118365164a3cfa824f43ebbb23a1
Rubén <ruben.sr.apple@gmail.com>
2024-04-22 11:55:02 +0200

    Create project in xcode

---
+++ doihavetime/doihavetime/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  doihavetime
+//
+//  Created by Ruben on 22/4/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ doihavetime/doihavetime/doihavetimeApp.swift
@@ -0,0 +1,17 @@
+//
+//  doihavetimeApp.swift
+//  doihavetime
+//
+//  Created by Ruben on 22/4/24.
+//
+
+import SwiftUI
+
+@main
+struct doihavetimeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
